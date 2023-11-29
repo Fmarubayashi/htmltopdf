b0505dbe0869d29198e71df87b26dd0500d5e363
Fernando Marubayashi <fmarubayashi@Fernandos-MacBook-Air.local>
2023-11-29 09:57:14 -0300

    adds images and css styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import HtmlToPdf from "./htmltopdf";
+import logo from "./logo.svg";
 
 function App() {
   const { getDataUrl, downloadPdf } = HtmlToPdf();
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <button onClick={() => console.log(getDataUrl())}>Get PDF here!</button>
         <br />
+        <img src={logo} alt="logo" width={120} />
         <button onClick={downloadPdf}>
           Or click here to download the PDF!
         </button>
--- src/htmltopdf.tsx
+++ src/htmltopdf.tsx
@@ -1,12 +1,19 @@
 import { jsPDF } from "jspdf";
+import { pathImg } from "./constants";
 
 const HtmlToPdf = () => {
-  const doc = new jsPDF();
-  const htmlString =
-    "<b>This is bold text</b>, <i>this is italic text</i>.<br />Here are some special characters: &copy; &lt;3 &amp;.<br />This is a new line.";
+  const doc = new jsPDF("p", "mm", [297, 210]);
+  const htmlString = `
+    <div style="font-size: 5px; color: red; display: flex; flex-direction: row; align-items: center;">
+      <p style="color: blue"><b>This is bold text</b>, <i>this is italic text</i>.</p>
+      <p>Here are some special characters: &copy; &lt;3 &amp;</p>
+      This is a new line.
+    </div>
+    <img src=${pathImg} width="100" height="66"/>
+`;
   doc.html(htmlString, {
-    x: 10,
-    y: 10,
+    margin: [27, 16, 27, 16],
+    autoPaging: "text",
   });
 
   function downloadPdf() {
